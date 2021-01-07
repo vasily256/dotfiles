65513fcb03d09665f611bdac08398a80f6412458
Vasily Markov <VMarkov@bellintegrator.com>
2021-01-07 22:50:52 +0300

    Updated i3status configs

--- usr/local/bin/i3status.sh
+++ usr/local/bin/i3status.sh
@@ -41,18 +41,38 @@ function remove_holder {
   echo "$json_array" | jq "del(.[] | (select(.instance==\"$instance\")))"
 }
 
-function update_json_array {
-    local json="{ \"full_text\": \"$2\", \"color\": \"#00FF00\" }"
-    json_array=$(update_holder "$1" "$json")
-}
+function fill_holder {
 
+    local holder_name="$1"
+    local yellow_level="$2"
+    local red_level="$3"
+    local str="$4"
+    local number
+
+    if [ -z $5 ] ; then
+        number=${str%.*}
+    else
+        number="$5"
+    fi
+
+    local color="#00FF00"
+
+    if [ $number -gt $red_level ] ; then
+        color="#FF0000"
+    elif [ $number -gt $yellow_level ] ; then
+        color="#FFFF00"
+    fi
+    local json="{ \"full_text\": \"$str\", \"color\": \"$color\" }"
+    json_array=$(update_holder "$holder_name" "$json")
+}
 
 i3status | (read line; echo "$line"; read line ; echo "$line" ; read line ; echo "$line" ; while true
 do
   read line
   json_array="$(echo $line | sed -e 's/^,//')"
-  update_json_array holder__cpu_freq $(lscpu | awk '/^CPU.MHz/ {print $3 " " $4}')
-  update_json_array holder__cpu_temp $(sensors | awk '/^Tctl/ {print $2 " " $3}')
+  fill_holder    holder__cpu_freq    2000 2800    $(($(cat /sys/devices/system/cpu/cpufreq/policy0/scaling_cur_freq)/1000))
+  fill_holder    holder__cpu_temp    60 80        $(sensors | awk '/^Tctl/ {print $2 " " $3}')
+  fill_holder    holder__ram         50 60        $(free -g | awk '/^Mem/ {print $3"/"$2}')    $(free -g | awk '/^Mem/ {print $3}')
   echo ",$json_array" 
 done)
 
